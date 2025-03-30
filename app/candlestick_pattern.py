afb16725ea644dc4a73e9b1c932e6766110969bf
johnhuitsai <caiqizhe@gmail.com>
2025-03-30 21:22:53 +0800

    feat(candlestick): 重构蜡烛图模式识别
    - 重写 CandlestickPattern 类，简化模式定义和匹配逻辑
    - 使用 pandas_ta 库替换自定义模式匹配算法
    - 更新 get_candlestick_patterns 函数，支持更多蜡烛图模式
    - 修改匹配成功后的输出信息，使用模式标签代替类名

--- app/candlestick_pattern.py
+++ app/candlestick_pattern.py
@@ -12,10 +12,25 @@ class CandlestickPattern:
         self.column = column
 
     def match(self, stock, prices, df):
+        """
+        判断给定股票的最近几个交易日中是否出现了特定的K线形态。
+
+        :param stock: 股票代码，用于标识特定的股票。
+        :param prices: 股票价格数据，通常包括开、高、低、收等价格信息。
+        :param df: 包含股票历史数据的DataFrame，至少包括['open', 'high', 'low', 'close']四个列。
+        :return: 布尔值，表示是否匹配到了指定的K线形态。
+        """
+        # 获取最近5个交易日的数据，以便进行K线形态识别
         recent_df = df.tail(5)
+
+        # 使用技术分析库ta，计算指定K线形态
         recent_df = ta.cdl_pattern(recent_df['open'], recent_df['high'], recent_df['low'], recent_df['close'],
                                    name=self.name)
+
+        # 筛选出符合特定K线形态的行，即该列的值不等于0
         pattern = recent_df[recent_df[self.column] != 0]
+
+        # 如果存在匹配的K线形态，则返回True，否则返回False
         return not pattern.empty
 
 
