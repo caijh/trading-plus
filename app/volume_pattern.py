b5326198eed4ea2f61f7813b689828de618a1da9
johnhuitsai <caiqizhe@gmail.com>
2025-03-30 22:29:45 +0800

    feat(volume_pattern): 改进 Chaikin A/D 指标判断逻辑
    - 在 match 函数中添加对 ADOSC 指标的计算和判断
    -增加对最新成交量和前一个成交量的处理
    - 更新判断条件，支持价格上升和下降时的 ADOSC 指标比较- 添加函数注释，说明其参数和返回值

--- app/volume_pattern.py
+++ app/volume_pattern.py
@@ -88,19 +88,44 @@ class ADOSC:
         self.label = 'Chaikin A/D'
 
     def match(self, stock, prices, df):
+        """
+        判断给定股票是否满足特定的交易条件。
+
+        通过计算股票的最新价格和成交量，以及使用累积/派发指标(ADOSC)来评估股票的买卖压力，
+        进而判断股票是否处于一个可能的买入或卖出状态。
+
+        参数:
+        - stock: 股票标识符，用于识别特定的股票。
+        - prices: 包含股票历史价格的列表，每个元素是一个字典，至少包含close和volume字段。
+        - df: 包含股票价格和成交量的数据框，用于计算ADOSC指标。
+
+        返回:
+        - 如果股票满足特定的交易条件则返回True，否则返回False。
+        """
+        # 获取最新价格信息
         price = prices[-1]
+        # 将最新成交量转换为浮点数
         latest_volume = float(price['volume'])
+        # 如果最新成交量不大于0，则不进行后续判断
         if not latest_volume > 0:
             return False
 
+        # 计算ADOSC指标
         adosc = ta.adosc(df['high'], df['low'], df['close'], df['volume'])
+        # 获取最新的ADOSC值和前一个ADOSC值
         latest_adosc = adosc.iloc[-1]
         pre_adosc = adosc.iloc[-2]
 
+        # 获取前一个价格信息
         pre_price = prices[-2]
         close_price = float(price['close'])
         pre_close_price = float(pre_price['close'])
-        return latest_adosc > pre_adosc and close_price > pre_close_price
+        # 判断最新ADOSC是否大于前一个ADOSC，且最新收盘价是否高于或低于前一个收盘价
+        # 如果A/D线上升的同时，价格也在上升，则说明上升趋势被确认，产生买入信号
+        # 如果A/D线上升的同时，价格在下降，二者产生背离，说明价格的下降趋势减弱，有可能反转回升
+        print(
+            f'Stock {stock["code"]}: latest_adosc={latest_adosc}, pre_adosc={pre_adosc}, close_price={close_price}, pre_close_price={pre_close_price}')
+        return latest_adosc > pre_adosc and (close_price > pre_close_price or close_price < pre_price)
 
 def get_volume_patterns():
     """
