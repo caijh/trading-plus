b4691049177635dc28a55e78405f4d5741c33b27
caijh <caiqizhe@gmail.com>
2025-06-08 21:37:18 +0800

    refactor(analysis): 重构股票分析数据保存逻辑
    - 提取 save_analyzed_stock 函数，减少代码重复
    - 添加空列表判断，避免不必要的数据库操作
    -优化支撑位和阻力位计算，确保数值类型一致
    -调整数据库会话管理，提高事务效率

--- analysis/service.py
+++ analysis/service.py
@@ -16,25 +16,33 @@ def save_analyzed_stocks(stocks):
     stocks (list): 包含股票数据的列表，每个股票数据是一个字典，包含股票的代码、名称
                    和其他分析数据如模式、支撑位和阻力位。
     """
-    # 开始一个数据库会话
+
+    if len(stocks) == 0:
+        return
+
     # 把分析过股票插入数据中，根据code删除原有的，再插入AnalyzedStock对应的表中
     with db.session.begin():
         for stock in stocks:
             try:
-                db.session.query(AnalyzedStock).filter_by(code=stock["code"]).delete()
-                analyzed_stock = AnalyzedStock(
-                    code=stock["code"],
-                    name=stock["name"],
-                    exchange=stock["exchange"],
-                    patterns=stock.get("patterns", []),
-                    support=stock.get("support"),
-                    resistance=stock.get("resistance"),
-                    price=stock.get("price", None)
-                )
-                db.session.add(analyzed_stock)
-                print(f"Add {analyzed_stock} to AnalyzedStock")
+                save_analyzed_stock(stock)
             except Exception as e:
                 print(f"处理 stock 出错: {stock['code']}, 错误信息: {e}")
+        db.session.commit()
+
+
+def save_analyzed_stock(stock):
+    db.session.query(AnalyzedStock).filter_by(code=stock["code"]).delete()
+    analyzed_stock = AnalyzedStock(
+        code=stock["code"],
+        name=stock["name"],
+        exchange=stock["exchange"],
+        patterns=stock.get("patterns", []),
+        support=stock.get("support"),
+        resistance=stock.get("resistance"),
+        price=stock.get("price", None)
+    )
+    db.session.add(analyzed_stock)
+    print(f"Add {analyzed_stock} to AnalyzedStock")
 
 
 def analyze_stock(stock, k_type=KType.DAY, signal=1):
@@ -159,8 +167,8 @@ def calculate_support_resistance(stock, df):
 
     n_digits = 3 if stock['stock_type'] == 'Fund' else 2
     # 计算最终的支撑位和阻力位
-    s = round(min(latest_data['S1'], latest_data['S2'], latest_data['S3']), n_digits)
-    r = round(min(latest_data['R1'], latest_data['R2'], latest_data['R3']), n_digits)
+    s = round(float(min(latest_data['S1'], latest_data['S2'], latest_data['S3'])), n_digits)
+    r = round(float(min(latest_data['R1'], latest_data['R2'], latest_data['R3'])), n_digits)
 
     # 打印计算结果
     print(f'{stock["code"]} calculate_support_resistance Support = {s}, Resistance = {r}')
