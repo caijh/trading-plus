28c439a0da40eb3c62c94a441afa8c06cc66267f
caijh <caiqizhe@gmail.com>
2025-06-17 10:38:56 +0800

    feat(stock): 添加股票信息缓存功能
    - 在 get_stock_info 和 get_stock_prices 函数中添加 Redis 缓存
    - 新增 ak_stock_zh_a_daily 函数，并实现缓存功能
    - 优化 get_adj_factor_from_akshare 函数，使用新增的缓存函数

--- stock/service.py
+++ stock/service.py
@@ -1,9 +1,12 @@
+import json
 from enum import Enum
+from io import StringIO
 
 import akshare as ak
 import pandas as pd
 
 from environment.service import env_vars
+from extensions import redis_client
 from request.service import http_get_with_retries
 
 
@@ -25,8 +28,15 @@ def get_stock(code):
     stock: 如果请求成功且数据有效，则返回股票信息，否则返回None。
     """
     # 构造请求URL，包含股票代码
+    value = redis_client.get(f'Trading-Plus:Stock:{code}')
+    if value is not None:
+        return json.loads(value)
+
     url = f'{env_vars.TRADING_DATA_URL}/stock?code={code}'
-    return http_get_with_retries(url, 3, None)
+    stock = http_get_with_retries(url, 3, None)
+    if stock is not None:
+        redis_client.set(f'Trading-Plus:Stock:{code}', json.dumps(stock), 30)
+    return stock
 
 
 def get_stock_prices(code, k_type=KType.DAY):
@@ -41,13 +51,23 @@ def get_stock_prices(code, k_type=KType.DAY):
     list: 如果请求成功，返回包含股票价格数据的列表；如果请求失败或不支持的k_type，则返回空列表。
     """
     # 当请求的是日K线数据时，构造请求URL并发送请求
+
+    prices = redis_client.get(f'Trading-Plus:Stock:{code}:{k_type}')
+    if prices is not None:
+        return json.loads(prices)
+
     if k_type == KType.DAY:
         url = f'{env_vars.TRADING_DATA_URL}/stock/price/daily?code={code}'
 
         if env_vars.DEBUG:
             print(f'Get stock price from {url} , code = {code}, k_type = {k_type}')
 
-        return http_get_with_retries(url, 3, [])
+        prices = http_get_with_retries(url, 3, [])
+
+        if len(prices) > 0:
+            redis_client.set(f'Trading-Plus:Stock:{code}:{k_type}', json.dumps(prices), 90)
+
+        return prices
     # 如果k_type不是DAY，直接返回空列表，表示不支持的k_type
     return []
 
@@ -67,6 +87,16 @@ def get_adj_factor(stock, start_date: str, end_date: str):
         raise RuntimeError(f"不支持的股票交易所：{exchange}")
 
 
+def ak_stock_zh_a_daily(symbol: str, start_date: str, end_date: str, adjust=""):
+    daily = redis_client.get(f'Trading-Plus:Stock:{symbol}:{start_date}:{end_date}:{adjust}')
+    if daily is not None:
+        return pd.read_json(StringIO(daily.decode('utf-8')))
+
+    daily = ak.stock_zh_a_daily(symbol=symbol, start_date=start_date, end_date=end_date, adjust=adjust)
+    if daily is not None:
+        redis_client.set(f'Trading-Plus:Stock:{symbol}:{start_date}:{end_date}:{adjust}', daily.to_json(), 60 * 60)
+    return daily
+
 def get_adj_factor_from_akshare(symbol: str, start_date: str, end_date: str) -> pd.DataFrame:
     """
     通过 AkShare 获取某只 A 股的复权因子（基于收盘价的前复权计算）
@@ -79,14 +109,14 @@ def get_adj_factor_from_akshare(symbol: str, start_date: str, end_date: str) -> 
     """
     # 获取原始（不复权）价格
     try:
-        df_raw = ak.stock_zh_a_daily(symbol=symbol, start_date=start_date, end_date=end_date, adjust="")  # 不复权
+        df_raw = ak_stock_zh_a_daily(symbol=symbol, start_date=start_date, end_date=end_date, adjust="")  # 不复权
         df_raw = df_raw.reset_index().rename(columns={"date": "date", "close": "close_raw"})
     except Exception as e:
         raise RuntimeError(f"获取原始股价失败：{e}")
 
     # 获取前复权价格
     try:
-        df_qfq = ak.stock_zh_a_daily(symbol=symbol, start_date=start_date, end_date=end_date, adjust="qfq")  # 前复权
+        df_qfq = ak_stock_zh_a_daily(symbol=symbol, start_date=start_date, end_date=end_date, adjust="qfq")  # 前复权
         df_qfq = df_qfq.reset_index().rename(columns={"date": "date", "close": "close_qfq"})
     except Exception as e:
         raise RuntimeError(f"获取前复权股价失败：{e}")
