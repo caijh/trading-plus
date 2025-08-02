b2f945ecb6a0c9bc07fafdbbeafc9b9c7a0e42cb
caijh <caiqizhe@gmail.com>
2025-08-02 16:30:07 +0800

    refactor(analysis): 移除股票分析中的冗余代码
    - 删除了注释掉的代码块，该代码块用于处理 SSE、SZSE 和 HKEX 交易所的股票
    - 优化了股票分析流程，现在只在 patterns 数量大于 0 时返回特定消息

--- analysis/routes.py
+++ analysis/routes.py
@@ -59,11 +59,6 @@ def analysis_index():
     if stock is None:
         return jsonify({'msg': 'stock not found'}), 404
 
-    # if stock['exchange'] in {'SSE', 'SZSE', 'HKEX'}:
-    #     analyze_stock(stock, k_type=KType.DAY, signal=1)
-    #     if len(stock['patterns']) == 0:
-    #         return jsonify({'code': 0, 'msg': 'Index pattern not match, analysis_index_task not run.'}), 200
-
     analyze_stock(stock, k_type=KType.DAY, signal=-1)
     if len(stock['patterns']) > 0:
         return jsonify({'code': 0, 'msg': 'Index pattern not match, analysis_index_task not run.'}), 200
