30483d2226fd907613e69d6418bebef28cb84528
caijh <caiqizhe@gmail.com>
2025-08-08 12:16:11 +0800

    refactor(analysis): 优化股票趋势判断逻辑
    - 修改了股票趋势判断条件，增加了当前价格的比较
    - 新的判断逻辑更准确地反映了股票的短期趋势

--- analysis/service.py
+++ analysis/service.py
@@ -529,7 +529,8 @@ def calculate_support_resistance_by_turning_points(stock, df, window=5):
     stock['direction'] = 'UP' if upping else 'DOWN'
 
     if len(turning_up_points) > 1:
-        stock['trending'] = 'UP' if turning_up_points.iloc[-1]['low'] > turning_up_points.iloc[-2]['low'] else 'DOWN'
+        stock['trending'] = 'UP' if current_price > turning_up_points.iloc[-1]['low'] > turning_up_points.iloc[-2][
+            'low'] else 'DOWN'
         # stock['turning_up_point_1'] = turning_up_points.iloc[-1].name.strftime('%Y-%m-%d')
         # stock['turning_up_point_2'] = turning_up_points.iloc[-2].name.strftime('%Y-%m-%d')
     else:
