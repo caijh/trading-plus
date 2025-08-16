40e566f0032ef40f9aa6b0f54a1248ef41ea6619
caijh <caiqizhe@gmail.com>
2025-08-16 21:47:34 +0800

    feat(indicator): 添加 PVI 和 NVI 指标
    - 在 base.py 中添加了趋势确认和背离的通用方法
    - 新增 nvi.py 文件，实现 NVI 指标的功能- 新增 pvi.py 文件，实现 PVI 指标的功能
    - 两个新指标均继承自 Indicator 类，并实现了各自的趋势确认和背离判断逻辑

--- indicator/base.py
+++ indicator/base.py
@@ -1,4 +1,37 @@
+import pandas as pd
+
+
 class Indicator:
 
     def match(self, stock, df, trending, direction):
+        return False
+
+    def trend_confirmation(self, series: pd.Series, trend):
+        """
+        判断 PVI 的趋势确认信号
+        """
+        # PVI 上升，表明在放量情况下股价上涨，是牛市信号
+        latest = series.iloc[-1]
+        prev = series.iloc[-2]
+        if trend == "bullish":
+            return latest > prev
+        elif trend == "bearish":
+            return latest < prev
+
+        return False
+
+    def divergence(self, series: pd.Series, divergence):
+        """
+        判断 PVI 的背离信号
+        """
+        # PVI 下降，表明在放量情况下股价下跌，是熊市信号
+        latest = series.iloc[-1]
+        prev = series.iloc[-2]
+        if divergence == 'bullish':
+            # 底背离：价格下跌但 PVI 上涨，暗示买盘力量增强
+            return latest > prev
+        elif divergence == 'bearish':
+            # 顶背离：价格上涨但 PVI 下跌，暗示卖盘力量增强
+            return latest < prev
+
         return False
