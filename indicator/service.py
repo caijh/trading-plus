15fcf3e8ded96d22edbb2cc6635f65e628bcd0d9
caijh <caiqizhe@gmail.com>
2025-08-25 03:06:40 +0800

    feat(indicator): 添加看涨和看跌全量指标并调整SAR指标的配置
    - 新增看涨全量指标函数 get_bullish_all_volume_patterns()
    - 新增看跌全量指标函数 get_bearish_all_volume_patterns()
    - 更新 SAR 指标的配置，使用新的全量指标
    -优化代码结构，导入新增的指标类

--- indicator/service.py
+++ indicator/service.py
@@ -1,8 +1,17 @@
+from indicator.adoc import ADOSC
+from indicator.adx import ADX
+from indicator.ar import AR
+from indicator.aroon import AROON
 from indicator.chaikin import Chaikin
 from indicator.cmf import CMF
+from indicator.confirm.macd import MACD
 from indicator.confirm.rsi import RSI
+from indicator.kvo import KVO
 from indicator.mfi import MFI
+from indicator.nvi import NVI
 from indicator.obv import OBV
+from indicator.pvi import PVI
+from indicator.vpt import VPT
 
 
 def get_breakthrough_up_volume_pattern():
@@ -47,6 +56,43 @@ def get_oversold_volume_patterns():
     ]
 
 
+def get_bullish_all_volume_patterns():
+    return [
+        ADX(1),
+        ADOSC(1),
+        AR(1),
+        AROON(1),
+        Chaikin(1),
+        CMF(1),
+        KVO(1),
+        MFI(1),
+        NVI(1),
+        OBV(1),
+        PVI(1),
+        RSI(1),
+        MACD(1),
+        VPT(1)
+    ]
+
+
+def get_bearish_all_volume_patterns():
+    return [
+        ADX(-1),
+        ADOSC(-1),
+        AR(-1),
+        AROON(-1),
+        Chaikin(-1),
+        CMF(-1),
+        KVO(-1),
+        MFI(-1),
+        NVI(-1),
+        OBV(-1),
+        PVI(-1),
+        RSI(-1),
+        MACD(-1),
+        VPT(-1)
+    ]
+
 def get_overbought_volume_patterns():
     return [RSI(-1), CMF(-1), MFI(-1)]
 
@@ -61,8 +107,8 @@ volume_registry = {
         -1: [Chaikin(-1)]
     },
     'SAR': {
-        1: get_breakthrough_up_volume_pattern(),
-        -1: get_breakthrough_down_volume_pattern()
+        1: [NVI(1), OBV(1)],
+        -1: [NVI(-1), OBV(-1)]
     },
     'BIAS': {
         1: get_oversold_volume_patterns(),
