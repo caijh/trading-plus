4cfd8de69bf50aef49139bbd0a1413a98a121c6d
caijh <caiqizhe@gmail.com>
2025-08-29 23:39:10 +0800

    refactor(volume_registry): 调整交易量指标的顺序
    - 调整了 VolumeRegistry 中交易量指标的顺序
    -将 AR、OBV、VPT 和 MACD 指标的顺序进行了重新排列
    - 优化了指标的组织结构，以便于更好的管理和使用

--- indicator/volume_registry.py
+++ indicator/volume_registry.py
@@ -19,15 +19,15 @@ def get_breakthrough_up_volume_pattern():
         ADL(1),
         ADOSC(1),
         ADX(1),
-        VPT(1),
-        CMF(1),
-        OBV(1),
+        AR(1),
         AROON(1),
-        MACD(1),
         Chaikin(1),
+        CMF(1),
         KVO(1),
         NVI(1),
-        AR(1)
+        OBV(1),
+        VPT(1),
+        MACD(1),
     ]
 
 
@@ -36,16 +36,15 @@ def get_breakthrough_down_volume_pattern():
         ADL(-1),
         ADOSC(-1),
         ADX(-1),
-        VPT(-1),
-        CMF(-1),
-        OBV(-1),
+        AR(-1),
         AROON(-1),
-        MACD(-1),
-        ADL(-1),
         Chaikin(-1),
+        CMF(-1),
         KVO(-1),
         NVI(-1),
-        AR(-1)
+        OBV(-1),
+        VPT(-1),
+        MACD(-1),
     ]
 
 
